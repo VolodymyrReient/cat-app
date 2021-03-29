c221f38a233075f8191729371244c71dcf895ff4
Volodymyr Reient <4reient4@gmail.com>
2021-03-29 09:27:18 +0300

    added link in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,10 @@ function Header() {
     return (
         <nav className="blue darken-4">
             <div className="nav-wrapper">
-                <a href="!#" className="brand-logo">
+                <a
+                    href="https://github.com/VolodymyrReient/cat-app"
+                    className="brand-logo"
+                >
                     React Cats
                 </a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
